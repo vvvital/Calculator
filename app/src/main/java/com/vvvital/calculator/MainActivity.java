c4574242128f316b02b9630f91dfbc6b569cf2b1
vvvital <vvvital>
2022-04-28 17:42:03 +0300

    create added button

--- app/src/main/java/com/vvvital/calculator/MainActivity.java
+++ app/src/main/java/com/vvvital/calculator/MainActivity.java
@@ -2,13 +2,31 @@ package com.vvvital.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.GnssAntennaInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+    int operand1;
+    int operand2;
+    int result;
+    String operator;
+
+    String number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void onClick(View view){
+
+    }
+
 }

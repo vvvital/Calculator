ccb1ca0826bfd85f1eccb5de9f21fe7fc9ca9c7f
vvvital <vvvital>
2022-04-28 18:16:06 +0300

    added back button

--- app/src/main/java/com/vvvital/calculator/MainActivity.java
+++ app/src/main/java/com/vvvital/calculator/MainActivity.java
@@ -2,10 +2,13 @@ package com.vvvital.calculator;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    String operation="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +17,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onClick(View view){
+    public void onClick(View view) {
+        Button button = (Button) view;
+        operation = operation.concat(button.getText().toString());
+        view();
+    }
 
+    public void onClickEquals(View view){
+
+    }
+
+    public void onClickBack(View view){
+        if (operation.length()>0) {
+            operation = operation.substring(0, operation.length() - 1);
+        }
+        view();
+    }
+
+    public void view() {
+        TextView textView = findViewById(R.id.console);
+        textView.setText(operation);
     }
 
 }

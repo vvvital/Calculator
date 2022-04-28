f22d41ab50449d7e8fb43dbb8ec8882e8cba299a
vvvital <vvvital>
2022-04-28 17:45:06 +0300

    clean main.java

--- app/src/main/java/com/vvvital/calculator/MainActivity.java
+++ app/src/main/java/com/vvvital/calculator/MainActivity.java
@@ -1,22 +1,11 @@
 package com.vvvital.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.location.GnssAntennaInfo;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import java.util.Scanner;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    int operand1;
-    int operand2;
-    int result;
-    String operator;
-
-    String number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
